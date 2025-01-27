a0e0a5953fcc8a6cd9f3e29a5d3805bfe2657fe1
vivek <vivekrandhawa6@gmail.com>
2025-01-27 18:52:32 +0530

    Adding hashrouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Context from "./context/StateContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
@@ -8,9 +8,9 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Context>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Context>
   </StrictMode>
 );
