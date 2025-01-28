83fb5719c057934e936391d62ef04b0d86980bb3
vivek-patel-here <146237401+vivek-patel-here@users.noreply.github.com>
2025-01-28 19:19:13 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Context from "./context/StateContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
@@ -8,9 +8,9 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Context>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Context>
   </StrictMode>
 );
