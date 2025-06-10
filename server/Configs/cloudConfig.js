c36c8b23ac73f742f7800e78ce8ab99283945b84
vivek <vivekrandhawa6@gmail.com>
2025-06-10 11:50:53 +0530

    configuring .env

--- server/Configs/cloudConfig.js
+++ server/Configs/cloudConfig.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 
 // Configure Cloudinary with your account details
 cloudinary.config({
-  cloud_name: 'dhmnfhipn',
-  api_key: '614921894965577',
-  api_secret: 'pHhE8VjYYVbFMR0eb262G4Nwn_Y',
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret:process.env.API_SECRET,
 });
 
 module.exports = cloudinary;
